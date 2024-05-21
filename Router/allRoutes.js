494d7cf7435272f3091fae75a8f94aad3efd2311
CynthiaVl <cynthia.vanlaer@gmail.com>
2024-05-21 16:54:23 +0200

    header-footer

---
+++ Router/allRoutes.js
@@ -0,0 +1,10 @@
+import Route from "./Route.js";
+
+//Définir ici les routes
+export const allRoutes = [
+    new Route("/", "Accueil", "/pages/home.html"),
+    new Route("/games", "Tous les jeux", "/pages/games.html"),
+];
+
+//Le titre s'affiche comme ceci : Route.titre = websitename
+export const websiteName = "GameStore";
